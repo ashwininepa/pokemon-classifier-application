c38e04c404febdb5af1fc276a378966b6c06d531
Ashwini Visave <ashwini.visave@nepa.com>
2025-03-30 23:43:52 +0200

    Function to classify image using loaded model

--- app/model.py
+++ app/model.py
@@ -6,3 +6,9 @@ def load_model():
     Load the Pokémon classification model.
     """
     return pipeline("image-classification", model="fufufukakaka/pokemon_image_classifier")
+
+def classify_image(image, model):
+    """
+    Classify the given image using the loaded model.
+    """
+    return model(image)
