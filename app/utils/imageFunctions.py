6950617abef179aceb952d57ed759088f27ee22d
Ashwini Visave <ashwini.visave@nepa.com>
2025-03-31 13:34:17 +0200

    Add an extra step to resize image in preprocessing (just to be sure and check if model performance improves)

--- app/utils/imageFunctions.py
+++ app/utils/imageFunctions.py
@@ -7,4 +7,5 @@ def preprocess_image(image_data):
     Preprocess the image data for classification.
     """
     image = Image.open(io.BytesIO(image_data)).convert("RGB")
+    image = image.resize((224, 224))  # Resize to 224x224
     return image
