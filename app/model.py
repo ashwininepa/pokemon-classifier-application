0158e45214df4e883d530ea80671932e5ec791e4
Ashwini Visave <ashwini.visave@nepa.com>
2025-03-31 13:32:23 +0200

    Add extra parameter use_fast to use pipeline

--- app/model.py
+++ app/model.py
@@ -4,8 +4,10 @@ from transformers import pipeline
 def load_model():
     """
     Load the Pokémon classification model.
+    For this task I am using a pre-trained model from Hugging Face.
+    The model is taken from the link 'https://huggingface.co/fufufukakaka/pokemon_image_classifier/blob/main/README.md?library=transformers'
     """
-    return pipeline("image-classification", model="fufufukakaka/pokemon_image_classifier")
+    return pipeline("image-classification", model="fufufukakaka/pokemon_image_classifier", use_fast=True)
 
 def classify_image(image, model):
     """
